462a12b92ad6d1ced9791d8ab4a1b1eb250b2480
znacer99 <naceurez95@gmail.com>
2023-10-26 10:26:38 +0000

    /hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def hello_hbnb():
     """Route page"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def index():
     """ /hbnb route """
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
