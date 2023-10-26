d99c3fa5a8751e2ad8861080256d5c950d12b372
znacer99 <naceurez95@gmail.com>
2023-10-26 10:59:57 +0000

    3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""python route"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """/ route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    """/hbnb route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """/c/<text>"""
+    return 'C {:s}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/Python/<text>', strict_slashes=False)
+def pythoniscool(text):
+    """/python/<text>"""
+    return 'Python {:s}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
