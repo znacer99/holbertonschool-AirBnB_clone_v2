7d2d9417f17dba2bb52bad89f2bc3c3d26827746
znacer99 <naceurez95@gmail.com>
2023-10-26 10:37:49 +0000

    c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#/usr/bin/python3
+"""Flask route model"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """/ route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    """/hbnb route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """/c route"""
+    return 'C {:s}'.format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
