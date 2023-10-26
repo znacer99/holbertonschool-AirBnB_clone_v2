59ef05f512dc9774d650d431fe284f2392db2495
znacer99 <naceurez95@gmail.com>
2023-10-26 10:13:36 +0000

    hello hbnb

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Model Route Flask v0.00.0 """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Route page"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
