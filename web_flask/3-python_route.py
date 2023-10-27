18163b7a76aadd94dc67683eb2eed87558913cc6
znacer99 <naceurez95@gmail.com>
2023-10-27 07:49:30 +0000

    3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""python route"""
+""" Flask route model """
 from flask import Flask
 
 
@@ -8,26 +8,26 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hbnb():
-    """/ route"""
+    """ / route  """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def index():
-    """/hbnb route"""
+    """ /hbnb route """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """/c/<text>"""
+    """ /c route """
     return 'C {:s}'.format(text.replace('_', ' '))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/Python/<text>', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text):
-    """/python/<text>"""
+    """ /c route """
     return 'Python {:s}'.format(text.replace('_', ' '))
 
 
